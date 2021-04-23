a97c1cd60217a110960aa046a8f221b9128fc14a
Alejandro <alemandoa@gmail.com>
2021-04-23 14:26:16 -0500

    Update TestAbstracto.java

--- src/test/java/test/TestAbstracto.java
+++ src/test/java/test/TestAbstracto.java
@@ -21,7 +21,7 @@ public class TestAbstracto {
 			new String [] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"},
 			"Alfabeto latino");
 	public static Fabula fabula = new Fabula("pensamiento", "La tortuga y la liebre", "Esopo", 2, 
-			"no se debe uno burlar de los demás, ni presumir o ser vanidoso", "Cuento corto");
+			"no se debe uno burlar de los demas, ni presumir o ser vanidoso", "Cuento corto");
 	
 	public static Libro libro = new Libro("Creacion", "El resplandor", "Stephen King", 599, "N/A", "Doubleday", "Primera", "Escrito largo");
 	
